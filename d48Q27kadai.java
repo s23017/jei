435266e728d35f871e0bcc2531d5bb57ebfce374
s23017 <s23017@std.it-college.ac.jp>
2024-05-24 11:56:35 +0900

    Q27 commit

---
+++ d48Q27kadai.java
@@ -0,0 +1,12 @@
+// s23017
+// Q27を無限ループにならないように修正
+
+public class d48Q27kadai {
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 6) {
+			System.out.println(i);
+			break;
+		}
+	}
+}
