d947f53fccee99684db9030a69ffd8837458bf07
s23017 <s23017@std.it-college.ac.jp>
2024-05-24 10:37:18 +0900

    d48Q22 commit

---
+++ d48Q22kadai.java
@@ -0,0 +1,14 @@
+// s23017
+// d48Q22のコードをコンパイルエラーしないように修正
+
+enum Connection {OFFLINE, ONLINE}
+
+class d48Q22kadai {
+	public static void main(String[] args){
+		Connection s = Connection.OFFLINE;
+		if (s == Connection.OFFLINE) {
+			s = Connection.ONLINE;
+		}
+		System.out.println(s);
+	}
+}
