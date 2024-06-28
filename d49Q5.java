a00f11105b237116cb27e05ce0659ac6b7a0b512
s23017 <s23017@std.it-college.ac.jp>
2024-06-28 02:42:45 +0000

    d49Q5 commit

---
+++ d49Q5.java
@@ -0,0 +1,25 @@
+public class d49Q5 {
+	public static void main(String[] args) {
+		int limit = Integer.parseInt(args[0]);
+		int base1 = Integer.parseInt(args[1]);
+		int base2 = Integer.parseInt(args[2]);
+
+		int common = base1 * base2;
+
+		for (int i=1; i<=limit; i++) {
+			if (i % common == 0) {
+				System.out.println(i + ":base1,base2");
+				continue;
+			}
+			if (i % base1 == 0) {
+				System.out.println(i + ":base1");
+				continue;
+			}
+			if (i % base2 == 0) {
+				System.out.println(i + ":base2");
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
---
+++ d49Q5kadai.java
@@ -0,0 +1,49 @@
+//s23017
+//
+
+public class d49Q5kadai {
+    public static void main(String[] args) {
+        int limit = Integer.parseInt(args[0]);
+        int base1 = Integer.parseInt(args[1]);
+        int base2 = Integer.parseInt(args[2]);
+        int base3 = Integer.parseInt(args[3]);
+
+        int common12 = base1 * base2;
+        int common13 = base1 * base3;
+        int common23 = base2 * base3;
+        int common123 = base1 * base2 * base3;
+
+        for (int i = 1; i <= limit; i++) {
+            if (i % common123 == 0) {
+                System.out.println(i + ":base1,base2,base3");
+                continue;
+            }
+            if (i % common12 == 0) {
+                System.out.println(i + ":base1,base2");
+                continue;
+            }
+            if (i % common13 == 0) {
+                System.out.println(i + ":base1,base3");
+                continue;
+            }
+            if (i % common23 == 0) {
+                System.out.println(i + ":base2,base3");
+                continue;
+            }
+            if (i % base1 == 0) {
+                System.out.println(i + ":base1");
+                continue;
+            }
+            if (i % base2 == 0) {
+                System.out.println(i + ":base2");
+                continue;
+            }
+            if (i % base3 == 0) {
+                System.out.println(i + ":base3");
+                continue;
+            }
+            System.out.println(i + ":");
+        }
+    }
+}
+
