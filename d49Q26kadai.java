b5066f6039344995e197b2757c2e774342967b54
s23017 <s23017@std.it-college.ac.jp>
2024-07-12 03:10:17 +0000

    d49Q26kadai push

---
+++ d49Q26kadai.java
@@ -0,0 +1,10 @@
+public class d49Q26kadai {
+	public static void main(String[] args) {
+		d49Q26kadai obj = new d49Q26kadai();
+		obj.show();
+	}
+
+	void show(double n) {
+		System.out.println("value is " + n);
+	}
+}
