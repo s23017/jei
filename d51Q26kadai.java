953e4a90668a24ea49db5666bc90ed3c1a835af4
s23017 <s23017@std.it-college.ac.jp>
2024-07-19 03:30:55 +0000

    d51Q26kadai.java push

---
+++ d51Q26kadai.java
@@ -0,0 +1,14 @@
+// s23017
+// 「->」演算子を使ったコード
+
+public class d51Q26kadai {
+	public static  void main(String[] args) {
+		int n = 1;
+		String s = switch (n) {
+			case 1 -> "one";
+			case 2 -> "two";
+			default -> "?";
+		};
+		System.out.println(s);
+	}
+}
