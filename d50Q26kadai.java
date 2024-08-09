e61bdc88ce08708d3de1d5a246e1f813598a151e
s23017 <s23017@std.it-college.ac.jp>
2024-08-09 01:37:36 +0000

    d52Q5 push

--- d50Q26kadai.java
+++ d50Q26kadai.java
@@ -1,8 +1,5 @@
 // s23017
-// 列挙型の課題
-// Q26をアレンジして、列挙型で定義したConditionの要素を全てを出力する課題です
-// その場合、Condition.values().lengthで要素の数を取得できる
-// そして、Condition.values()[i]で要素値を取得できます
+// Q26をアレンジして、列挙型で定義したConditionの要素を全てを出力する
 enum Condition {ROAD, MUD, SNOW}
 
 class d50Q26kadai {
